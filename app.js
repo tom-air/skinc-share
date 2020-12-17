7013befa0657c921e5dc1b61d9ccde3a99807dd0
Tom <tom@hello-air.com>
2020-12-17 13:42:13 +0800

    remove limit

--- app.js
+++ app.js
@@ -36,8 +36,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.use(config.subAppDomain, express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+// app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(helmet());
 app.use(compression());
